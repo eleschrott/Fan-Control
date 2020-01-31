d3502febb89607a40494eb9467636f2c66daddc7
WolfgangEs <32031099+eleschrott@users.noreply.github.com>
2020-01-31 19:13:29 +0100

    add brackets to the macro SetPWM32kHz()

--- VisualStudio/FanControl/FanControl.h
+++ VisualStudio/FanControl/FanControl.h
@@ -46,7 +46,7 @@ constexpr auto FAN_FIX      = 63; // PWM value for save start
 #endif
 
 // Macro to set pre scale to one -> 32kHz PWM
-#define SetPWM32kHz() TCCR0B = TCCR0B & 0b11111000 | 0b001
+#define SetPWM32kHz() TCCR0B = TCCR0B & (0b11111000 | 0b001)
 
 uint16_t readTemp(void);
 void setFanSpeed(uint8_t speed);
